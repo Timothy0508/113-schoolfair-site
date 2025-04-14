b80be4a9f402a0f6d8dd58c734ad4554f1551dbd
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2025-04-14 09:43:38 +0800

    feat: apply api calling and basic ui layout

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -3,7 +3,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./styles.module.css";
 import React from "react";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { faArrowRight, faCartShopping, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const API_URL = 'https://ncapi.dns-dynamic.net';
+const request = axios.create({
+    baseURL: API_URL,
+});
+const fetchData = async () => {
+    try {
+        //Try to get menu by calling "/get-menu" endpoint
+        const response = await request.get("/get-menu");
+        return response.data;
+    } catch (err) {
+        if (axios.isAxiosError(err) && err.response?.status === 404) {
+            // Not an error, just no menu
+            return null;
+        }
+        throw err; // Re-throw for the outer catch to handle
+    }
+};
 
 export default function ShoppingPage() {
     return (
@@ -16,6 +35,20 @@ export default function ShoppingPage() {
                     </button>
                 </p>
             </header>
+            <section className={styles.menuItems} id="menuItems"></section>
+            <section className={styles.menuActions}>
+                <button className={styles.menuActionButton}>
+                    Clear cart
+                    <FontAwesomeIcon icon={faXmark} className={styles.menuActionButtonIcon} />
+                </button>
+                <button className={styles.menuActionButton}>
+                    Checkout
+                    <FontAwesomeIcon icon={faArrowRight} className={styles.menuActionButtonIcon} />
+                </button>
+            </section>
+            <footer className={styles.footer}>
+                <p className={styles.footerText}>© 2023 113 Stand</p>
+            </footer>
         </>
     )
 }
